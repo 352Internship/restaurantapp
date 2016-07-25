cf8b312dc56bec5b9acb0301413bdbdcccf9f91b
LT Carbonell <lt.carbonell@gmail.com>
2016-07-25 18:11:33 -0400

    Add constructor to query db for menu item selected

--- client/app/item/item.controller.js
+++ client/app/item/item.controller.js
@@ -3,8 +3,34 @@
 (function(){
 
 class ItemComponent {
-  constructor() {
-    this.message = 'Hello';
+  constructor($state, MenuItem) {
+    this.$state = $state;
+    this.MenuItem = MenuItem;
+    this.item = {};
+
+    var cart = [];
+    this.getShoppingCart(cart);
+  }
+
+
+  $onInit() {
+    var self = this;
+    this.MenuItem.get({id: this.$state.params.itemId }, function(data) {
+      console.log(data);
+      self.item = data;
+    });
+  }
+
+  getShoppingCart(cart) {
+    if (localStorage.getItem("cart")) {
+      cart = JSON.parse(localStorage.getItem("cart"));
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+
+  addToCart(item) {
+    this.cart.append(item);
+    console.log("cart", this.cart);
   }
 }
 
