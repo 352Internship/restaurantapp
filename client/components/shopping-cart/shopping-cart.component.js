86674d666c5de05ae518660509f22cf03c2ab6d4
LT Carbonell <lt.carbonell@gmail.com>
2016-07-29 11:09:14 -0400

    Re-route cart button to view-cart

--- client/components/shopping-cart/shopping-cart.component.js
+++ client/components/shopping-cart/shopping-cart.component.js
@@ -7,7 +7,7 @@ function shoppingCartController(shoppingCart) {
 angular.module('served2App')
   .component('shoppingCart', {
     template: `
-      <button ng-click="ShoppingCart.shoppingCart.getItems()" type="button" class="btn btn-default btn-lg">
+      <button ui-sref="view-cart" type="button" class="btn btn-default btn-lg">
         <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span> Cart
       </button>
     `,
